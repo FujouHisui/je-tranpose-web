dc9a2b0ad6718092c4410eb77882d0aafeec53dc
Hisui <i@hisui.tech>
2023-01-10 19:46:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      v-navigation-drawer
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      drawer: null,
+    };
+  },
+});
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-textarea name="input-1" label="JE sheet" auto-grow></v-textarea>
+      <v-card-actions>
+        <v-btn elevation="2">降</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn elevation="2">升</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+
+  components: {},
+});
+</script>
